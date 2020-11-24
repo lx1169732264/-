9ca26d4290bd35d91a9f8a56686c4cab1afd3b1d
lx <1169732264@qq.com>
2020-11-24 09:54:29 +0800

    20.11.24

--- src/lc_0500/Lc_0402_removeKdigits.java
+++
@@ -1,9 +0,0 @@
-package lc_0500;
-
-public class Lc_0402_removeKdigits {
-    public String removeKdigits(String num, int k) {
-        for (int i = 0; i < num.length(); i++) {
-            int i1 = Integer.parseInt(String.valueOf(num.charAt(i)));
-        }
-    }
-}
